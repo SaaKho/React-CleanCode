3fcdec1240d5ec690a601b4229d78d40c80a6194
Malik Saadan Khokhar <109228868+SaaKho@users.noreply.github.com>
2024-08-19 12:24:47 +0500

    Add files via upload

---
+++ Hoisting.js
@@ -0,0 +1,20 @@
+//Hoisting in Javascript is 
+
+
+let a =1;
+console.log(a);
+
+// output = 1
+
+console.log(b);
+let b =1;
+
+//output = undefined 
+//Javascript moves the declaration to the top 
+//Not the Definition
+
+//If i do
+
+console.log(c);
+//Output c is not defined
+
